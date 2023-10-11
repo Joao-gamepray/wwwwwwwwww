613acfa0711d16c59bf74a73b43614506a0fc62a
Joao-gamepray <110043744+Joao-gamepray@users.noreply.github.com>
2023-10-11 08:58:24 -0300

    aaaaaaaaaa
    aaaaaaaaaaaaaaaaaa

---
+++ Trabalho-main rogerao/main.js
@@ -0,0 +1,19 @@
+function tocaSomMario(){
+    document.querySelector('#som_tecla_mario').play();
+}
+document.querySelector('.tecla_mario').onclick=tocaSomMario;
+
+function tocaSomShowDoMilhao(){
+    document.querySelector('#som_tecla_show').play();
+}
+document.querySelector('.tecla_show').onclick=tocaSomShowDoMilhao;
+
+function tocaSomPlantao(){
+    document.querySelector('#som_tecla_plantao').play();
+}
+document.querySelector('.tecla_plantao').onclick=tocaSomPlantao
+
+function tocaSomBrasil(){
+    document.querySelector('#som_tecla_brasil').play();
+}
+document.querySelector('.tecla_brasil').onclick=tocaSomBrasil;
